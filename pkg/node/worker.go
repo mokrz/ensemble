0ed21b703b8022a23663156f3f1c0f7e145a3000
kmokr <cmokricky@protonmail.com>
2020-03-07 19:27:45 -0800

    Add a constructor for worker node structs

--- pkg/node/worker.go
+++ pkg/node/worker.go
@@ -1,5 +1,16 @@
 package node
 
+import "github.com/containerd/containerd"
+
 type Worker struct {
 	Node
+}
+
+func NewWorker(cfg Config, ctr *containerd.Client) (w *Worker) {
+	return &Worker{
+		Node: Node{
+			Cfg: cfg,
+			Ctr: ctr,
+		},
+	}
 }
