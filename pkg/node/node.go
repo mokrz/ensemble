c7c798cfabac53201062af28e598b5453ea03149
kmokr <cmokricky@protonmail.com>
2020-03-07 19:27:02 -0800

    Add 'Create' methods for some container primitives

--- pkg/node/node.go
+++ pkg/node/node.go
@@ -1,5 +1,63 @@
 package node
 
+import (
+	"context"
+	"errors"
+
+	"github.com/containerd/containerd"
+	"github.com/containerd/containerd/cio"
+	"github.com/containerd/containerd/oci"
+)
+
 type Node struct {
 	Cfg Config
+	Ctr *containerd.Client
+}
+
+func (n Node) CreateImage(ctx context.Context, imgRef string) (err error) {
+	_, pullErr := n.Ctr.Pull(ctx, imgRef, containerd.WithPullUnpack)
+
+	if pullErr != nil {
+		return errors.New("Node failed to pull image " + imgRef + " with error: " + pullErr.Error())
+	}
+
+	return nil
+}
+
+func (n Node) CreateContainer(ctx context.Context, imgRef, containerName, ssName string) (containerID string, err error) {
+	image, getImgErr := n.Ctr.GetImage(ctx, imgRef)
+
+	if getImgErr != nil {
+		return "", errors.New("Node failed to get image " + imgRef + " with error: " + getImgErr.Error())
+	}
+
+	container, createErr := n.Ctr.NewContainer(
+		ctx,
+		containerName,
+		containerd.WithImage(image),
+		containerd.WithNewSnapshot(ssName, image),
+		containerd.WithNewSpec(oci.WithImageConfig(image)),
+	)
+
+	if createErr != nil {
+		return "", errors.New("Node failed to create container " + containerName + " with error: " + createErr.Error())
+	}
+
+	return container.ID(), nil
+}
+
+func (n Node) CreateTask(ctx context.Context, containerID string) (err error) {
+	container, loadContainerErr := n.Ctr.LoadContainer(ctx, containerID)
+
+	if loadContainerErr != nil {
+		return errors.New("Node failed to load container " + containerID + " with error: " + loadContainerErr.Error())
+	}
+
+	_, newTaskErr := container.NewTask(ctx, cio.NewCreator(cio.WithStdio))
+
+	if newTaskErr != nil {
+		return errors.New("Node failed to create task with error: " + newTaskErr.Error())
+	}
+
+	return nil
 }
