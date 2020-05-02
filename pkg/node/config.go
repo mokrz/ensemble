fcb7c6157444506265cb1a3902d96456f9c38f03
Ubuntu <mokrz@github.com>
2020-05-02 09:10:01 +0000

    Implement `Node.LoadConfig`

--- pkg/node/config.go
+++ pkg/node/config.go
@@ -1,6 +1,29 @@
 package node
 
+import (
+	"encoding/json"
+	"errors"
+	"os"
+)
+
 type Config struct {
-	Name           string
-	ContainerdPath string
+	Name           string `json:"name"`
+	ContainerdPath string `json:"containerd_path"`
+}
+
+func LoadConfig(path string) (cfg *Config, err error) {
+	file, openErr := os.Open(path)
+
+	if openErr != nil {
+		return nil, errors.New("LoadConfig: os.Open failed with error: " + openErr.Error())
+	}
+
+	var buff Config
+	decodeErr := json.NewDecoder(file).Decode(&buff)
+
+	if decodeErr != nil {
+		return nil, errors.New("LoadConfig: Decoder.Decode failed with error: " + decodeErr.Error())
+	}
+
+	return &buff, nil
 }
